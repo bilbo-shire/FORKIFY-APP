1d76de6b382a387a0bf37d03bc8191614d3fcb0b
bilbo-shire <88598758+bilbo-shire@users.noreply.github.com>
2021-08-15 00:18:32 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '20378f5b-cf46-42f1-99c7-3876a10b49c3';
+export const MODAL_CLOSE_SEC = 2.5;
